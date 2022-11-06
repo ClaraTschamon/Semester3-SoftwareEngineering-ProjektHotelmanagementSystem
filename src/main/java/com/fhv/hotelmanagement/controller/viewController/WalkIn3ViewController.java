5a6515022d45321b62e5aae8173e4aa99a391c53
JSamJA <116830949+JSamJA@users.noreply.github.com>
2022-11-06 18:10:50 +0100

    Samuel WalkIn3ViewController

--- src/main/java/com/fhv/hotelmanagement/controller/viewController/WalkIn3ViewController.java
+++ src/main/java/com/fhv/hotelmanagement/controller/viewController/WalkIn3ViewController.java
@@ -1,7 +1,26 @@
 package com.fhv.hotelmanagement.controller.viewController;
 
+import com.fhv.hotelmanagement.MainApplication;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class WalkIn3ViewController {
 
+    @FXML
+    public void onBackButtonClickedPayment(ActionEvent e) throws IOException, URISyntaxException {
+        MainApplication.getMainController().loadIntoContentArea("walk-in-2");
+    }
 
+    @FXML
+    public void onCancelButtonClickedPayment(ActionEvent e) throws IOException, URISyntaxException {
+        MainApplication.getMainController().loadIntoContentArea("home");
+    }
 
+    @FXML
+    public void onSaveButtonClicked(ActionEvent e) throws IOException, URISyntaxException {
+        MainApplication.getMainController().loadIntoContentArea("home");
+    }
 }
