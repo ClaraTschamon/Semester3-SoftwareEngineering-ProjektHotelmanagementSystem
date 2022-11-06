1258b3e9a8d6ea032e394aba9c2742a7c667d6fa
ClaraTschamon <clara.tschamon@students.fhv.at>
2022-11-06 20:50:40 +0100

    anpassung

--- src/main/java/com/fhv/hotelmanagement/controller/viewController/WalkIn1ViewController.java
+++ src/main/java/com/fhv/hotelmanagement/controller/viewController/WalkIn1ViewController.java
@@ -1,10 +1,21 @@
 package com.fhv.hotelmanagement.controller.viewController;
 
 import com.fhv.hotelmanagement.MainApplication;
+import com.fhv.hotelmanagement.controller.useCaseController.WalkInUseCaseController;
+import com.fhv.hotelmanagement.domainModel.Room;
+import com.fhv.hotelmanagement.domainModel.RoomCategory;
+import com.fhv.hotelmanagement.persistence.dataMapper.RoomCategoryDataMapper;
+import com.fhv.hotelmanagement.persistence.dataMapper.RoomDataMapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -14,9 +25,20 @@ import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Properties;
+import java.util.ResourceBundle;
 
-public class WalkIn1ViewController {
+public class WalkIn1ViewController implements Initializable {
+
+    @FXML
+    private ComboBox<String> roomCategoryDropdown;
+
+    @FXML
+    private ComboBox<String> roomCategories;
+
+    @FXML
+    private ComboBox<String> roomNumberDropdown;
 
     @FXML
     private Text chooseRoom;
@@ -76,12 +98,14 @@ public class WalkIn1ViewController {
 
         ArrayList<Room> allRooms = RoomDataMapper.getAll();
         ObservableList<String> rooms = FXCollections.observableArrayList();
-        String selectedRoomCategory = roomCategories.toString();
-        System.out.println("Selected Category: " + selectedRoomCategory);
+        //String selectedRoomCategory = roomCategories.toString();
+        //System.out.println("Selected Category: " + selectedRoomCategory);
         for(Room room : allRooms) {
+            rooms.add(Integer.toString(room.getNumber()));
+            /*
             if(room.getCategory().getName().equals(selectedRoomCategory)){
                 rooms.add(Integer.toString(room.getNumber()));
-            }
+            }*/
         }
         roomNumberDropdown.setItems(rooms);
 
