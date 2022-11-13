801878785e7e3295bc7f8720fd19c5d95d0b3e09
empireofdirth <aignerdominikender@hotmail.com>
2022-11-13 15:13:50 +0100

    Dominik validierung walk in 2 - 3

--- src/main/java/com/fhv/hotelmanagement/controller/viewController/WalkIn2ViewController.java
+++ src/main/java/com/fhv/hotelmanagement/controller/viewController/WalkIn2ViewController.java
@@ -36,31 +36,11 @@ public class WalkIn2ViewController {
     @FXML
     private TextField postalCode;
 
-    @FXML
-    private ComboBox<String> paymentMethod;
-
-    @FXML
-    private TextField creditCardNumber;
-
-    @FXML TextField verfuegernummer;
-
-    @FXML
-    private TextField billingCity;
-
-    @FXML
-    private TextField billingStreet;
-
-    @FXML
-    private TextField billingHouseNumber;
-
-    @FXML
-    private TextField billingPostalCode;
     private WalkInUseCaseController useCaseController;
-
 
     @FXML
     private void onNextButtonClicked(ActionEvent e) throws IOException {
-        try{
+        try {
             // TODO fill all attributes
             useCaseController.getCustomer().setFirstName(firstName.getText());
             useCaseController.getCustomer().setLastName(lastName.getText());
@@ -72,60 +52,177 @@ public class WalkIn2ViewController {
             useCaseController.getCustomer().setHouseNumber(houseNumber.getText());
             useCaseController.getCustomer().setPostalCode(postalCode.getText());
 
-            MainApplication.getMainController().loadIntoContentArea("walk-in-3");
-            WalkIn3ViewController walkIn3ViewController = MainApplication.getMainController().getCurrentFXMLLoader().getController();
-            walkIn3ViewController.setUseCaseController(useCaseController);
-        }catch(IOException | URISyntaxException exc){
-            System.out.println(exc.getMessage());
-        }
-    }
+            String textfieldFirstName = firstName.getText();
+            String textfieldLastName = lastName.getText();
+            String textfieldNationality = nationality.getText();
+            String textfieldPhoneNumber = phoneNumber.getText();
+            String textfieldEmail = email.getText();
+            String textfieldStreet = street.getText();
+            String textfieldHouseNumber = houseNumber.getText();
+            String textfieldCity = city.getText();
+            String textfieldPostalCode = postalCode.getText();
 
-    @FXML
-    private void onBackButtonClicked(ActionEvent e) throws IOException {
-        try{
-            MainApplication.getMainController().loadIntoContentArea("walk-in-1");
-        }catch(IOException | URISyntaxException exc){
-            System.out.println(exc.getMessage());
+            boolean firstNameIsValid = false;
+            boolean lastNameIsValid = false;
+            boolean nationalityIsValid = false;
+            boolean phoneNumberIsValid = false;
+            boolean emailIsValid = false;
+            boolean streetIsValid = false;
+            boolean houseNumberIsValid = false;
+            boolean cityIsValid = false;
+            boolean postalCodeIsValid = false;
+
+            if (!textfieldFirstName.isEmpty() && textfieldFirstName.matches("[a-zA-ZäÄöÖüÜß]*")) {
+                firstNameIsValid = true;
+            } else if(textfieldFirstName.isEmpty()) {
+                firstName.setStyle("-fx-text-inner-color: red");
+                firstName.setText("Pflichtfeld");
+            } else if(!textfieldFirstName.matches("[a-zA-ZäÄöÖüÜß]*")) {
+                firstName.setStyle("-fx-text-inner-color: red");
+                firstName.setText("inkorrekte Eingabe");
+            }
+
+            if (!textfieldLastName.isEmpty() && textfieldLastName.matches("[a-zA-ZäÄöÖüÜß]*")) {
+                lastNameIsValid = true;
+            } else if(textfieldLastName.isEmpty()) {
+                lastName.setStyle("-fx-text-inner-color: red");
+                lastName.setText("Pflichtfeld");
+            } else if(!textfieldLastName.matches("[a-zA-ZäÄöÖüÜß]*")) {
+                lastName.setStyle("-fx-text-inner-color: red");
+                lastName.setText("inkorrekte Eingabe");
+            }
+
+            if (!textfieldNationality.isEmpty() && textfieldNationality.matches("[a-zA-ZäÄöÖüÜß]*")) {
+                nationalityIsValid = true;
+            } else if(textfieldNationality.isEmpty()) {
+                nationality.setStyle("-fx-text-inner-color: red");
+                nationality.setText("Pflichtfeld");
+            } else if(!textfieldNationality.matches("[a-zA-ZäÄöÖüÜß]*")) {
+                nationality.setStyle("-fx-text-inner-color: red");
+                nationality.setText("inkorrekte Eingabe");
+            }
+
+            if (!textfieldPhoneNumber.isEmpty() && textfieldPhoneNumber.matches("[0-9]{7,15}")) {
+                phoneNumberIsValid = true;
+            } else if(textfieldPhoneNumber.isEmpty()) {
+                phoneNumber.setStyle("-fx-text-inner-color: red");
+                phoneNumber.setText("Pflichtfeld");
+            } else if(!textfieldPhoneNumber.matches("[0-9]*")) {
+                phoneNumber.setStyle("-fx-text-inner-color: red");
+                phoneNumber.setText("inkorrekte Eingabe");
+            } else if(textfieldPhoneNumber.length() > 7 || textfieldPhoneNumber.length() < 15) {
+                phoneNumber.setStyle("-fx-text-inner-color: red");
+                phoneNumber.setText("länge der Telefonnummer beachten");
+            }
+
+            if (!textfieldEmail.isEmpty() && textfieldEmail.matches("^(.+)@(.+)$")) {
+                emailIsValid = true;
+            } else if(textfieldEmail.isEmpty()) {
+                email.setStyle("-fx-text-inner-color: red");
+                email.setText("Pflichtfeld");
+            } else if(!textfieldEmail.matches("^(.+)@(.+)$")) {
+                email.setStyle("-fx-text-inner-color: red");
+                email.setText("inkorrekte Eingabe");
+            }
+
+            if (!textfieldStreet.isEmpty() && textfieldStreet.matches("[a-zA-ZäÄöÖüÜß]*")) {
+                streetIsValid = true;
+            } else if(textfieldStreet.isEmpty()) {
+                street.setStyle("-fx-text-inner-color: red");
+                street.setText("Pflichtfeld");
+            } else if(!textfieldStreet.matches("[a-zA-ZäÄöÖüÜß]*")) {
+                street.setStyle("-fx-text-inner-color: red");
+                street.setText("inkorrekte Eingabe");
+            }
+
+            if (!textfieldHouseNumber.isEmpty() && textfieldHouseNumber.matches("[0-9]*")) {
+                houseNumberIsValid = true;
+            } else if(textfieldHouseNumber.isEmpty()) {
+                houseNumber.setStyle("-fx-text-inner-color: red");
+                houseNumber.setText("Pflichtfeld");
+            } else if(!textfieldHouseNumber.matches("[0-9]*")) {
+                houseNumber.setStyle("-fx-text-inner-color: red");
+                houseNumber.setText("inkorrekte Eingabe");
+            }
+
+            if (!textfieldCity.isEmpty() && textfieldCity.matches("[a-zA-ZäÄöÖüÜß]*")) {
+                cityIsValid = true;
+            } else if(textfieldCity.isEmpty()) {
+                city.setStyle("-fx-text-inner-color: red");
+                city.setText("Pflichtfeld");
+            } else if(!textfieldCity.matches("[a-zA-ZäÄöÖüÜß]*")) {
+                city.setStyle("-fx-text-inner-color: red");
+                city.setText("inkorrekte Eingabe");
+            }
+
+            if (!textfieldPostalCode.isEmpty() && textfieldPostalCode.matches("[0-9]*")) {
+                postalCodeIsValid = true;
+            } else if(textfieldPostalCode.isEmpty()) {
+                postalCode.setStyle("-fx-text-inner-color: red");
+                postalCode.setText("Pflichtfeld");
+            } else if(!textfieldPostalCode.matches("[0-9]*")) {
+                postalCode.setStyle("-fx-text-inner-color: red");
+                postalCode.setText("inkorrekte Eingabe");
+            }
+
+                if (firstNameIsValid && lastNameIsValid && nationalityIsValid && phoneNumberIsValid &&
+                        emailIsValid && streetIsValid && houseNumberIsValid && cityIsValid && postalCodeIsValid) {
+                    MainApplication.getMainController().loadIntoContentArea("walk-in-3");
+                    WalkIn3ViewController walkIn3ViewController = MainApplication.getMainController().getCurrentFXMLLoader().getController();
+                    walkIn3ViewController.setUseCaseController(useCaseController);
+                }
+
+            } catch (IOException | URISyntaxException exc) {
+                System.out.println(exc.getMessage());
+            }
         }
+
+        @FXML
+        private void onBackButtonClicked (ActionEvent e) throws IOException {
+            try {
+                MainApplication.getMainController().loadIntoContentArea("walk-in-1");
+            } catch (IOException | URISyntaxException exc) {
+                System.out.println(exc.getMessage());
+            }
 //        mainApplication.load("Walk-In1");
 
-    }
-
-    @FXML
-    private void onCancelButtonClicked(ActionEvent e){
-        try{
-            MainApplication.getMainController().loadIntoContentArea("home");
-        }catch(IOException | URISyntaxException exc){
-            System.out.println(exc.getMessage());
         }
-        //alle Daten aus TextFields löschen? --> muss ausprobiert werden!
-    }
 
-    public void setUseCaseController(WalkInUseCaseController useCaseController) {
-        this.useCaseController = useCaseController;
-    }
+        @FXML
+        private void onCancelButtonClicked (ActionEvent e){
+            try {
+                MainApplication.getMainController().loadIntoContentArea("home");
+            } catch (IOException | URISyntaxException exc) {
+                System.out.println(exc.getMessage());
+            }
+            //alle Daten aus TextFields löschen? --> muss ausprobiert werden!
+        }
+
+        public void setUseCaseController (WalkInUseCaseController useCaseController){
+            this.useCaseController = useCaseController;
+        }
 
 
-    //Payment Page (Walk-In3)
-    @FXML
-    private void onBackButtonClickedPayment(ActionEvent e) throws IOException {
+        //Payment Page (Walk-In3)
+        @FXML
+        private void onBackButtonClickedPayment (ActionEvent e) throws IOException {
 //        mainApplication.load("Walk-In2");
-    }
+        }
 
 
-    @FXML
-    private void onCancelButtonClickedPayment(ActionEvent e){
-        //alle Textfelder löschen
-        //home menü laden
+        @FXML
+        private void onCancelButtonClickedPayment (ActionEvent e){
+            //alle Textfelder löschen
+            //home menü laden
 
-    }
+        }
 
 
-    @FXML
-    private void onSaveButtonClicked(ActionEvent e){
-        //daten in datenbank speichern und alle textfelder löschen
-        //      (ausprobieren ob es nicht automatisch gelöscht wird)
-        //home menü laden
+        @FXML
+        private void onSaveButtonClicked (ActionEvent e){
+            //daten in datenbank speichern und alle textfelder löschen
+            //      (ausprobieren ob es nicht automatisch gelöscht wird)
+            //home menü laden
 
         /*
         CreateCustomerUseCaseController useCaseController = new CreateCustomerUseCaseController();
@@ -139,5 +236,5 @@ public class WalkIn2ViewController {
         useCaseController.storeCustomer(customerEntity);
 
          */
+        }
     }
-}
--- src/main/java/com/fhv/hotelmanagement/controller/viewController/WalkIn3ViewController.java
+++ src/main/java/com/fhv/hotelmanagement/controller/viewController/WalkIn3ViewController.java
@@ -4,12 +4,34 @@ import com.fhv.hotelmanagement.MainApplication;
 import com.fhv.hotelmanagement.controller.useCaseController.WalkInUseCaseController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class WalkIn3ViewController {
     WalkInUseCaseController useCaseController;
+
+    @FXML
+    private TextField expirationDateTextfield;
+
+    @FXML
+    private TextField creditCardNumber;
+
+    @FXML
+    private TextField verfuegernummer;
+
+    @FXML
+    private TextField billingCity;
+
+    @FXML
+    private TextField billingStreet;
+
+    @FXML
+    private TextField billingHouseNumber;
+
+    @FXML
+    private TextField billingPostalCode;
 
     @FXML
     public void onBackButtonClickedPayment(ActionEvent e) throws IOException, URISyntaxException {
@@ -24,10 +46,107 @@ public class WalkIn3ViewController {
     @FXML
     public void onSaveButtonClicked(ActionEvent e) throws IOException, URISyntaxException {
         // TODO fill all attributes
+        String textfieldCreditCardNumber = creditCardNumber.getText();
+        String textfieldVerfuegerNummer = verfuegernummer.getText();
+        String textfieldBillingCity = billingCity.getText();
+        String textfieldBillingHouseNumber = billingHouseNumber.getText();
+        String textfieldBillingStreet = billingStreet.getText();
+        String textfieldBillingPostalCode = billingPostalCode.getText();
+        String textfieldLoc = expirationDateTextfield.getText();
 
+        boolean creditCardNumberIsValid = false;
+        boolean verfuegerNummerIsValid = false;
+        boolean billingCityIsValid = false;
+        boolean billingHouseNumberIsValid = false;
+        boolean billingStreetIsValid = false;
+        boolean billingPostalCodeIsValid = false;
+        boolean locIsValid = false;
 
-        useCaseController.save();
-        MainApplication.getMainController().loadIntoContentArea("home");
+        if (!textfieldCreditCardNumber.isEmpty() && textfieldCreditCardNumber.matches("[0-9]{8,19}")) {
+            creditCardNumberIsValid = true;
+        } else if(textfieldCreditCardNumber.isEmpty()) {
+            creditCardNumber.setStyle("-fx-text-inner-color: red");
+            creditCardNumber.setText("Pflichtfeld");
+        } else if(!textfieldCreditCardNumber.matches("[0-9]*")) {
+            creditCardNumber.setStyle("-fx-text-inner-color: red");
+            creditCardNumber.setText("inkorrekte eingabe");
+        } else if(textfieldCreditCardNumber.length() < 8 || textfieldCreditCardNumber.length() >19) {
+            creditCardNumber.setStyle("-fx-text-inner-color: red");
+            creditCardNumber.setText("die Kreditkartennummer hat 8 bis 19 Ziffern");
+        }
+
+        if (!textfieldVerfuegerNummer.isEmpty() && textfieldVerfuegerNummer.matches("[0-9]{3}")) {
+            verfuegerNummerIsValid = true;
+        } else if(textfieldVerfuegerNummer.isEmpty()) {
+            verfuegernummer.setStyle("-fx-text-inner-color: red");
+            verfuegernummer.setText("Pflichtfeld");
+        } else if(!textfieldVerfuegerNummer.matches("[0-9]*")) {
+            verfuegernummer.setStyle("-fx-text-inner-color: red");
+            verfuegernummer.setText("inkorrekte Eingabe");
+        } else if(textfieldVerfuegerNummer.length() < 3 || textfieldVerfuegerNummer.length() > 3) {
+            verfuegernummer.setStyle("-fx-text-inner-color: red");
+            verfuegernummer.setText("die Sichherheitsnummer hat 3 Ziffern");
+        }
+
+        if (!textfieldBillingCity.isEmpty() && textfieldBillingCity.matches("[a-zA-ZäÄöÖüÜß]*")) {
+            billingCityIsValid = true;
+        } else if(textfieldBillingCity.isEmpty()) {
+            billingCity.setStyle("-fx-text-inner-color: red");
+            billingCity.setText("Pflichtfeld");
+        } else if(!textfieldBillingCity.matches("[a-zA-ZäÄöÖüÜß]*")) {
+            billingCity.setStyle("-fx-text-inner-color: red");
+            billingCity.setText("inkorrekte Eingabe");
+        }
+
+        if (!textfieldBillingHouseNumber.isEmpty() && textfieldBillingHouseNumber.matches("[0-9]*")) {
+            billingHouseNumberIsValid = true;
+        } else if(textfieldBillingHouseNumber.isEmpty()) {
+            billingHouseNumber.setStyle("-fx-text-inner-color: red");
+            billingHouseNumber.setText("Pflichtfeld");
+        } else if(!textfieldBillingHouseNumber.matches("[0-9]*")) {
+            billingHouseNumber.setStyle("-fx-text-inner-color: red");
+            billingHouseNumber.setText("inkorrekte Eingabe");
+        }
+
+        if (!textfieldBillingStreet.isEmpty() && textfieldBillingStreet.matches("[a-zA-ZäÄöÖüÜß]*")) {
+            billingStreetIsValid = true;
+        } else if(textfieldBillingStreet.isEmpty()) {
+            billingStreet.setStyle("-fx-text-inner-color: red");
+            billingStreet.setText("Pflichtfeld");
+        } else if(!textfieldBillingStreet.matches("[a-zA-ZäÄöÖüÜß]*")) {
+            billingStreet.setStyle("-fx-text-inner-color: red");
+            billingStreet.setText("inkorrekte Eingabe");
+        }
+
+        if (!textfieldBillingPostalCode.isEmpty() && textfieldBillingPostalCode.matches("[0-9]{1,10}")) {
+            billingPostalCodeIsValid = true;
+        } else if(textfieldBillingPostalCode.isEmpty()) {
+            billingPostalCode.setStyle("-fx-text-inner-color: red");
+            billingPostalCode.setText("Pflichtfeld");
+        } else if(!textfieldBillingPostalCode.matches("[0-9]*")) {
+            billingPostalCode.setStyle("-fx-text-inner-color: red");
+            billingPostalCode.setText("inkorrekte Eingabe");
+        } else if(textfieldBillingPostalCode.length() < 1 || textfieldBillingPostalCode.length() > 10) {
+            billingPostalCode.setStyle("-fx-text-inner-color: red");
+            billingPostalCode.setText("die Postleitzahl");
+        }
+
+        if (!textfieldLoc.isEmpty() && textfieldLoc.matches("[0-1][1-9]/[0-9][0-9]")) {
+            locIsValid = true;
+        } else if(textfieldLoc.isEmpty()) {
+            expirationDateTextfield.setStyle("-fx-text-inner-color: red");
+            expirationDateTextfield.setText("Pflichtfeld");
+        } else if(!textfieldLoc.matches("[0-1][1-9]/[0-9][0-9]")) {
+            expirationDateTextfield.setStyle("-fx-text-inner-color: red");
+            expirationDateTextfield.setText("inkorrekte Eingabe");
+        }
+
+        if(creditCardNumberIsValid && verfuegerNummerIsValid && billingStreetIsValid &&
+        billingHouseNumberIsValid && billingCityIsValid && billingPostalCodeIsValid && verfuegerNummerIsValid) {
+            useCaseController.save();
+            MainApplication.getMainController().loadIntoContentArea("home");
+        }
+
     }
 
     public void setUseCaseController(WalkInUseCaseController useCaseController) {
