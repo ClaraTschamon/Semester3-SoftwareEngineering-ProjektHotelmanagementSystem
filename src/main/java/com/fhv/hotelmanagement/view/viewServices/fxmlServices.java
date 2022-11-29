0b31d8eab3577254271500fe5a074980a4670258
empireofdirth <aignerdominikender@hotmail.com>
2022-11-29 07:43:20 +0100

    Prompt text, hover, walki-in2-3, Stringvalidator

---
+++ src/main/java/com/fhv/hotelmanagement/view/viewServices/fxmlServices.java
@@ -0,0 +1,26 @@
+package com.fhv.hotelmanagement.view.viewServices;
+
+import javafx.event.EventHandler;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
+public class fxmlServices {
+
+    private void setTextColor(TextField textField, String color) {
+        textField.setStyle("-fx-text-inner-color: " + color);
+    }
+
+    private void setRequieredField(TextField textField) {
+        textField.setPromptText("Pflichtfeld");
+        textField.setStyle("-fx-prompt-text-fill: red");
+    }
+
+    private void setEventHandler(TextField textField){
+        textField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                setTextColor(textField, "black");
+            }
+        });
+    }
+}
