241f8518854728dd8d30a04979a9c2fc6d6bde3a
IdaMazinger <116703840+IdaMazinger@users.noreply.github.com>
2022-12-03 18:25:13 +0100

    ida checkout toRoom in BookedRooms anpassen

--- src/main/java/com/fhv/hotelmanagement/view/controller/useCaseController/CheckOutUseCaseController.java
+++ src/main/java/com/fhv/hotelmanagement/view/controller/useCaseController/CheckOutUseCaseController.java
@@ -3,9 +3,11 @@ package com.fhv.hotelmanagement.view.controller.useCaseController;
 
 import com.fhv.hotelmanagement.domain.domainController.DomainController;
 import com.fhv.hotelmanagement.domain.exceptions.BookingIsInvalidException;
+import com.fhv.hotelmanagement.view.DTOs.BookedRoomDTO;
 import com.fhv.hotelmanagement.view.DTOs.BookingDTO;
 import com.fhv.hotelmanagement.view.DTOs.InvoiceDTO;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class CheckOutUseCaseController {
@@ -21,7 +23,18 @@ public class CheckOutUseCaseController {
 
     public void save() {
         if (booking != null) {
+            LocalDate today = LocalDate.now();
             booking.setCheckOutDatetime(LocalDateTime.now());
+
+            for (BookedRoomDTO bookedRoomDTO : booking.getBookedRooms()) {
+                if (bookedRoomDTO.getFromDate().isAfter(today)) {
+                    bookedRoomDTO.setFromDate(null);
+                    bookedRoomDTO.setToDate(null);
+                } else if (bookedRoomDTO.getToDate().isAfter(today)) {
+                    bookedRoomDTO.setToDate(today);
+                }
+            }
+
             try {
                 DomainController.saveBooking(booking);
                 DomainController.saveInvoice(new InvoiceDTO(booking));
