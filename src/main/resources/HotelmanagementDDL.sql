0cb2c3c8ac487c737eac9cdf2eea4e525d26efae
JSamJA <116830949+JSamJA@users.noreply.github.com>
2022-12-07 12:20:17 +0100

    Samuel: Reservation wurde hinzugefügt

--- src/main/resources/HotelmanagementDDL.sql
+++ src/main/resources/HotelmanagementDDL.sql
@@ -43,12 +43,37 @@ CREATE TABLE customer(
 
 CREATE TABLE booking(
     booking_number LONG PRIMARY KEY AUTO_INCREMENT,
+    reservation_number LONG,
+    FOREIGN KEY (reservation_number) REFERENCES reservation(reservation_number),
     customer_number LONG,
     FOREIGN KEY (customer_number) REFERENCES customer(customer_number),
     arrival_date DATE,
     check_in_datetime DATETIME, //Zeitstempel wann man eingecheckt hat
     departure_date DATE,
     check_out_datetime DATETIME,
+    billing_street VARCHAR(255),
+    billing_house_number VARCHAR(255),
+    billing_postal_code VARCHAR(255),
+    billing_city VARCHAR(255),
+    billing_country VARCHAR(255),
+    comment VARCHAR(255),
+    payment_method VARCHAR(255),
+    credit_card_number VARCHAR(255),
+    expiration_date VARCHAR(5),
+    authorisation_number VARCHAR(255),
+    board_name VARCHAR(255),
+    FOREIGN KEY (board_name) REFERENCES board(board_name),
+    price_per_night_for_board INTEGER,
+    amount_guests INTEGER
+);
+
+CREATE TABLE reservation(
+    reservation_number LONG PRIMARY KEY AUTO_INCREMENT,
+    booking_number LONG,
+    FOREIGN KEY (booking_number) REFERENCES booking(booking_number),
+    creation_timestamp DATETIME, //Timestamp der Reservierungserstellung wegen Zahlungsfrist
+    arrival_date DATE,
+    departure_date DATE,
     billing_street VARCHAR(255),
     billing_house_number VARCHAR(255),
     billing_postal_code VARCHAR(255),
@@ -74,9 +99,27 @@ CREATE TABLE booked_room(
     to_date DATE
 );
 
+CREATE TABLE reserved_room(
+    reservation_number LONG,
+    FOREIGN KEY(reservation_number) REFERENCES reservation(reservation_number),
+    room_number INTEGER NOT NULL,
+    FOREIGN KEY(room_number) REFERENCES room(room_number),
+    from_date DATE,
+    to_date DATE
+);
+
 CREATE TABLE booked_room_category(
     booking_number LONG,
     FOREIGN KEY (booking_number) REFERENCES booking(booking_number),
+    room_category_name VARCHAR(255),
+    FOREIGN KEY (room_category_name) REFERENCES room_category(room_category_name),
+    booking_price_per_night INTEGER,
+    amount_room_category INTEGER
+);
+
+CREATE TABLE reserved_room_category(
+    reservation_number LONG,
+    FOREIGN KEY (reservation_number) REFERENCES reservation(reservation_number),
     room_category_name VARCHAR(255),
     FOREIGN KEY (room_category_name) REFERENCES room_category(room_category_name),
     booking_price_per_night INTEGER,
