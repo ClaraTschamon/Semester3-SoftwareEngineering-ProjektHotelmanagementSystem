41255283f7086cbc72ffd2deccada6e657efd656
JSamJA <116830949+JSamJA@users.noreply.github.com>
2022-12-14 20:17:21 +0100

    Samuel: Parsing getestet und fertig gestellt. Daten werden direkt eingespielt

--- src/main/java/com/fhv/hotelmanagement/view/viewServices/DepositService.java
+++ src/main/java/com/fhv/hotelmanagement/view/viewServices/DepositService.java
@@ -1,11 +1,27 @@
 package com.fhv.hotelmanagement.view.viewServices;
 
+import com.fhv.hotelmanagement.view.DTOs.DepositDTO;
+
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class DepositService {
+    private HashMap<String, DepositDTO> deposits = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
         DepositService depositService = new DepositService();
         depositService.parseData(depositService.convertData());
+
+//        Res#=12345#Date=01NOV2022#Amount=123.34;Date=01NOV2022#
+//        Res#=12346#Amount=50.0#IBAN=AT07123412341234123412;
+
+//        System.out.println(depositService.deposits.get("12345").getAmount());
+//        System.out.println(depositService.deposits.get("12345").getDate());
+//        System.out.println(depositService.deposits.get("12345").getIban());
+//        System.out.println(depositService.deposits.get("12346").getAmount());
+//        System.out.println(depositService.deposits.get("12346").getDate());
+//        System.out.println(depositService.deposits.get("12346").getIban());
     }
 
     public StringBuilder convertData() throws IOException {
@@ -20,21 +36,16 @@ public class DepositService {
         StringBuilder data = new StringBuilder();
 
         while ((line = bufferedReader.readLine()) != null) {
-//            System.out.println(line);
             data.append(line);
         }
-
         return data;
     }
 
     public void parseData(StringBuilder data){
-        String reservationNumber = "";
         String date = "";
         String amount = "";
         String iban = "";
-
-//        Res#=12345#Date=01NOV2022#Amount=123.34;Date=01NOV2022#
-//        Res#=12346#Amount=50.0#IBAN=AT07123412341234123412;
+        String reservationNumber = "";
 
         for(int i = 0; i<data.length();i++){
             if(data.charAt(i)=='R'){
@@ -49,7 +60,7 @@ public class DepositService {
 
             if(data.charAt(i)=='D'){
                 i+=5;
-                int y = i+8;
+                int y = i+9;
                 while (i<y){
                     date+=data.charAt(i);
                     i++;
@@ -76,6 +87,13 @@ public class DepositService {
                 System.out.println(iban);
             }
 
+            if(data.charAt(i)==';'){
+                deposits.put(reservationNumber, new DepositDTO(date,amount,iban, reservationNumber));
+                date = "";
+                amount = "";
+                iban = "";
+                reservationNumber = "";
+            }
         }
     }
 }
