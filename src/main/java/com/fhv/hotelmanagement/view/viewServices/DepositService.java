977e36be1d52431e296553ac7dbfa783aad1a411
JSamJA <116830949+JSamJA@users.noreply.github.com>
2022-12-14 18:40:55 +0100

    Samuel: Datenverarbeitung parseData Methode

--- src/main/java/com/fhv/hotelmanagement/view/viewServices/DepositService.java
+++ src/main/java/com/fhv/hotelmanagement/view/viewServices/DepositService.java
@@ -5,10 +5,10 @@ import java.io.*;
 public class DepositService {
     public static void main(String[] args) throws IOException {
         DepositService depositService = new DepositService();
-        depositService.convertData();
+        depositService.parseData(depositService.convertData());
     }
 
-    public void convertData() throws IOException {
+    public StringBuilder convertData() throws IOException {
         File file = new File("DatenBuchhaltung");
         System.out.println(file.exists());
 
@@ -23,10 +23,35 @@ public class DepositService {
             System.out.println(line);
             data.append(line);
         }
+
+        return data;
     }
 
-    public void parseData(String data){
+    public void parseData(StringBuilder data){
+        String reservationNumber = "";
+        String date = "";
+        String amount = "";
+        String IBAN = "";
+        String match = "";
 
+        for(int i = 0; i<data.length();i++){
+            System.out.println(data.charAt(i));
+            if(data.charAt(i)=='R'){
+                reservationNumber+=data.charAt(i+5);
+                reservationNumber+=data.charAt(i+6);
+                reservationNumber+=data.charAt(i+7);
+                reservationNumber+=data.charAt(i+8);
+                reservationNumber+=data.charAt(i+9);
+            }
 
+            if(data.charAt(i)=='D'){
+                date+=data.charAt(i+5);
+                date+=data.charAt(i+5);
+                date+=data.charAt(i+5);
+                date+=data.charAt(i+5);
+                date+=data.charAt(i+5);
+            }
+
+        }
     }
 }
